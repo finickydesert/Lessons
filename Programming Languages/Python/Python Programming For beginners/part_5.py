72535e5a133db0c43006ad44adb1297929fe5fb3
richard herist <rherist@gmail.com>
2019-09-05 10:57:38 -0400

    while the hurricane was going by slowly, i was working on this.

--- Programming Languages/Python/Python Programming For beginners/part_5.py
+++ Programming Languages/Python/Python Programming For beginners/part_5.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+#making a table
+print('{0:8} | {1:8}'.format("fruit", "quantity"))
+print('{0:8} | {1:8}'.format("apple",3))
+print('{0:8} | {1:8}'.format("orange", 10)
+#to control alignment use < for left, ^ for center, > right
+print('{0:8} | {1:<8}'.format("fruit", "how much?"))
+print('{0:8} | {1:<8}'.format("apple",3))
+print('{0:8} | {1:<8}'.format("orange", 10))
+answer = input("enter a name of a fruit: ")
+print("{} is a lovely fruit.".format(answer))
--- Programming Languages/Python/Python Programming For beginners/part_6.py
+++ Programming Languages/Python/Python Programming For beginners/part_6.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+#remember to have the function 'def' before the call
+#the formula for def function_name (parameter_name)
+""""you need to have a parameter or you eill get an error"""
+def say_hi(name = 'there'):
+    print('Hi {}!'.format(name))
+say_hi()
+say_hi('Bob')
+def say_bye(first, last = 'doe'):
+    print('hi {} {}!'.format(first, last))
+say_bye('jane')
+say_bye('john','smith')
