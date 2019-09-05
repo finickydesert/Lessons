1a85ec33da2901d1fbc00dc3e86f58d6bd714691
richard herist <rherist@gmail.com>
2019-09-04 21:20:12 -0400

    what i was working on while waiting for Hurrican Dorian to go by

---
+++ Programming Languages/Python/Python Programming For beginners/Part_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#a lot of printing is going to occur for this, this is from the chapter on how to printing
+pet = "cat"
+first_char = pet[0]
+d = 'dog'[0]
+print(pet)
+print(first_char)
+print("hi")
+pet_len = len(pet)
+print(pet_len)
+print(len(pet))
+print(len("dog"))
+rumi1 = "Don't grieve. Anything you lose comes round in another form."
+print(rumi1.lower)
+rumi2 = "Only from the heart Can you touch the sky."
+print(rumi2.upper)
+print('i '+ "love " + "python.")
+print("without"+"space")
---
+++ Programming Languages/Python/Python Programming For beginners/part_1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""i learned some of these scripts and techniques from python for beginners ISBN-13 978-1501000867 """
+pet = "cat"
+first3letters = "abc"
+first_three_letters = "abc"
+firstThreeLetters = "abc"
+rumiQuote = "Beauty surrounds us, but usually we need to be walking in a garden to know it."
+sentence_in_double = "she said \"well, that's great\""
+print(rumiQuote)
