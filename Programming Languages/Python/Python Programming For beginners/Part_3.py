fb22dbe78be6ee60fffa1d3f34bda8147b20f38e
richard herist <rherist@gmail.com>
2019-09-04 22:10:29 -0400

    while the hurricane was going by slowly, i was working on this.

--- Programming Languages/Python/Python Programming For beginners/Part_3.py
+++ Programming Languages/Python/Python Programming For beginners/Part_3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# this was to show concatination of variables
 first = "I"
 second = "love"
 third = "python"
--- Programming Languages/Python/Python Programming For beginners/part_4.py
+++ Programming Languages/Python/Python Programming For beginners/part_4.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""cannot explicitly convert int to str"""
+import webbrowser
+import urllib.parse
+try:
+   print("I {} python".format("love"))
+   print({0} {1} {2} {3}.format("I", "love", "you", 3000))
+except Exception as e:
+    url = 'http://stackoverflow.com/search?q=' + urllib.parse.quote('[python-3.x] %s' % e)
+    print(url)
+    webbrowser.open(url)
+#i got the original from: https://twitter.com/wittrup/status/697059127256817667
+#i modifyed it to point to tagged post for python 3 
