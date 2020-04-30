9f73f2a9d8d362246210dbc8d8e3c3d60849388e
richard <finickydesert@users.noreply.github.com>
2020-04-30 00:28:16 -0400

    Update calc_v2.py
    A critique on my code to correct a few things here

--- Programming Languages/Python/calc_v2.py
+++ Programming Languages/Python/calc_v2.py
@@ -9,18 +9,18 @@ error_catcher(
     num2 = float(input("input the second number"))
 
   while x == True:
-    user_input = input("to use this calulator, you must enter the following options exactlty:/nadd/nsubtract/nmultiply/ndivide/nandquit/nso, what do you want to do?")
+    user_input = input("to use this calulator, you must enter the following options exactly:/nadd/nsubtract/nmultiply/ndivide/nandquit/nso, what do you want to do?")
     if user_input == "add":
-        get_answer
+        get_answer()
         simple_result(num1 + num2)
     elif user_input == "subtract":
-        get_answer
+        get_answer()
         simple_result = num1 - num2
     elif user_input == "multiply":
-        get_answer
+        get_answer()
         simple_result = num1 * num2
     elif user_input == "divide":
-        get_answer
+        get_answer()
         simple_result = num1 / num2
     else: print("bye")
 
