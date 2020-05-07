c6f9426bfc429bbfc46781309ea9979d3e763281
richard <finickydesert@hotmail.com>
2020-05-07 15:23:12 -0400

    minor update

--- Tools/fresh install/Fedora/step 1.py
+++ Tools/fresh install/Fedora/step 1.py
@@ -1,3 +1,9 @@
 import os
 
-os.system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo; dnf update kernel*; mkdir /media/VirtualBoxGuestAdditions; mount -r /dev/cdrom /media/VirtualBoxGuestAdditions; dnf install gcc kernel-devel kernel-headers dkms make bzip2 perl libxcrypt-compat; dnf install gcc kernel-devel kernel-headers dkms make bzip2 perl libxcrypt-compat; export KERN_DIR; cd /media/VirtualBoxGuestAdditions; ./VBoxLinuxAdditions.run")
+os.system("flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo")
+os.system("sudo dnf update kernel*")
+os.system("mkdir /media/VirtualBoxGuestAdditions")
+os.system("mount -r /dev/cdrom /media/VirtualBoxGuestAdditions")
+os.system("sudo dnf install gcc kernel-devel kernel-headers dkms make bzip2 perl libxcrypt-compat")
+os.system("dnf install gcc kernel-devel kernel-headers dkms make bzip2 perl libxcrypt-compat")
+os.system("export KERN_DIR; cd /media/VirtualBoxGuestAdditions; ./VBoxLinuxAdditions.run")
