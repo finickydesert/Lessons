359d1cdda43345334e040035327a1892a642e739
richard <finickydesert@hotmail.com>
2020-05-18 22:42:19 -0400

    update

---
+++ Programming Languages/Python/name.py
@@ -0,0 +1,11 @@
+# this program said hi also maaaaybe as for a name.
+
+print("Hello world!")
+print("what's your name")
+name = input()
+print("it is good to meet you, " + name)
+print("the length of your name is: ")
+print(len(name))
+print("what is your age?") # switching topics
+age = input()
+print("you will be " + str(int(age) + 1) + " in a year.")
