2855167746cad6eb49518df864062c75c2149d8c
richard <finickydesert@hotmail.com>
2020-05-19 18:39:09 -0400

    i continued with the lessons

---
+++ Programming Languages/Python/for loop.py
@@ -0,0 +1,9 @@
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+name = 0
+for i in range(12, 16):
+    print('dude' + str(i))
+
---
+++ Programming Languages/Python/while name.py
@@ -0,0 +1,5 @@
+name = ''
+while name != 'your name':
+    print('please enter a name.')
+    name = input()
+print('thanks')
