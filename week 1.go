a4762678a687ee5f1bc1376493955bf6671005db
richard <finickydesert@users.noreply.github.com>
2020-11-03 12:30:06 -0500

    Create week 1.go
    taking https://www.coursera.org/learn/golang-getting-started/ while the python course calms down, it went from videos where 1+1 =2 to quantum physics for the exam with little build up practice. i will be going over the python course either on my lunch break or tomorrow night maybe...

---
+++ week 1.go
@@ -0,0 +1,24 @@
+//a quick summary of notes for this
+/*go tools 
+-go build: compiles the program, arguments can be a list of .go files or packages. it creates a executable in the same name as the .go file
+-go doc: prints documentation for a package
+-go fmt: format source code files
+-go get: downloads packages and installs them
+-go list: list all the installed packages
+-go run compiles .go files and runs the executable
+-Go test: it runs tests.
+*/
+/*varibles 
+case sensitive can use numbers
+must have name, type, and declerations
+you can can declare many on the same line seperated by comma
+
+
+
+
+*/
+package main
+import "fmt"
+func main() {
+  fmt.Printf("Hello, World"\n)
+}
