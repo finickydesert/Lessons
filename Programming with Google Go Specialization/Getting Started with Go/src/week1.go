021e445e9a49738b89e8b2065c6ee82768683489
richard <finickydesert@users.noreply.github.com>
2020-11-09 18:52:48 -0500

    Update week1.go
    testing this out

--- Programming with Google Go Specialization/Getting Started with Go/src/week1.go
+++ Programming with Google Go Specialization/Getting Started with Go/src/week1.go
@@ -1,5 +1,6 @@
 //a quick summary of notes for this
-/*go tools
+/*
+go tools
 -go build: compiles the program, arguments can be a list of .go files or packages. it creates a executable in the same name as the .go file
 -go doc: prints documentation for a package
 -go fmt: format source code files
@@ -20,6 +21,8 @@ you can declare variables using type alias
 ex:
 var temp Celsius
 var pid IDnum
+*/
+/*
 == initializing
 either var x in = 100 or var x = 100
 specify type may eliminate future problems
@@ -37,4 +40,37 @@ package main
 import "fmt"
 func main() {
   fmt.Printf("Hello, World")
+  fmt.Printf("a quick summary of notes for this week") 
+  fmt.Printf("go tools")
+  fmt.Printf("go build: compiles the program, arguments can be a list of .go files or packages. it creates a executable in the same name as the .go file")
+  fmt.Printf("go doc: prints documentation for a package")
+  fmt.Printf("go fmt: format source code files")
+  fmt.Printf("go get: downloads packages and installs them")
+  fmt.Printf("go list: list all the installed packages")
+  fmt.Printf("go run compiles .go files and runs the executable")
+  fmt.Printf("Go test: it runs tests.")
+  fmt.Printf("*******") 
+  fmt.Printf("case sensitive can use numbers")
+  fmt.Printf("must have name, type, and declerations")
+  fmt.Printf("you can can declare many on the same line seperated by comma")
+  fmt.Printf("type declerations may improve clarity and define a alias")
+  fmt.Printf("ex: ")
+  fmt.Printf("type Celsius float64")
+  fmt.Printf("type IDum int")
+  fmt.Printf("you can declare variables using type alias")
+  fmt.Printf("ex: ")
+  fmt.Printf("var temp Celsius")
+  fmt.Printf("var pid IDnum")
+  fmt.Printf("== initializing")
+  fmt.Printf("either var x in = 100 or var x = 100")
+  fmt.Printf("specify type may eliminate future problems")
+  fmt.Printf("uninitalized variables have 0 value")
+  fmt.Printf("ex:")
+  fmt.Printf("var x int")
+  fmt.Printf("var x string")
+  fmt.Printf("-short var declerations :=")
+  fmt.Printf("ex")
+  fmt.Printf("x := 100")
+  fmt.Printf("can only do inside a function")
+           
 }
