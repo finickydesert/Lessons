258722cdb5e272f1e95f241816c49202b2f61f17
richard <finickydesert@users.noreply.github.com>
2020-11-09 18:09:24 -0500

    Update week2.go
    uploading pointers notes within a golang file

--- Programming with Google Go Specialization/Getting Started with Go/src/week2.go
+++ Programming with Google Go Specialization/Getting Started with Go/src/week2.go
@@ -1,3 +1,14 @@
-\**
-hold
-**/
+\*
+pointers:
+a pointer is a address to data in memory
+2 operators &, *
+the & operator returns the address of a function or var
+the * operator returns the data at am address
+opposites of each other
+alternate way to create a var is by using the new() function. new() creates amd returns a pointer to a variable.
+buy default variable is initalized to zero.
+*/
+\*
+Variable Scope:
+
+*/
