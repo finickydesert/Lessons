5d8e02fae608cc6d820f0934c88df31e109d75ee
richaheris <>
2020-12-23 14:17:29 -0500

    correcting some things

--- Lua/crash_course_in.lua
+++ Lua/crash_course_in.lua
@@ -1,2 +1,8 @@
---lua docs or specification on version would be helpful because it's ... a quagmire if you look for one version and end up with everyother version. like 5.3 but google is showing results for 4.9. 
-
+print(" got this from: https://www.youtube.com/watch?v=kgiEF1frHQ8 , https://github.com/pohka/Lua-Beginners-Guide , https://www.lua.org/manual/5.4/ ")
+print("lua docs or specification on version would be helpful because it's ... a quagmire if you look for one version and end up with everyother version. like 5.3 but google is showing results for 4.9. ")
+print("you can make a comment by prefixing ' -- ' ")
+print("this is what's going to print")
+print("local (var name) is a way to assign vars but, as the prefix states local with in the function not outside, which would be global")
+print("usually you don't have to specify the type.")
+print("to concatinate you would need to  ' .. ' (kind of like +) between the 2 objects you want to concatinate together")
+print("if then and end are the basics of the if statement. if elseif else end are what you could use for validating like elif in python")
